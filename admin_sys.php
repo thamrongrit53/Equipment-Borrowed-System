4c4da82ea187cd44deaa9b9b699ad704d897a944
thamrongrit53 <47946165+thamrongrit53@users.noreply.github.com>
2020-06-08 21:24:27 +0700

    admin page

--- admin_sys.php
+++ admin_sys.php
@@ -8,8 +8,8 @@
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
   <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js"></script>
-  <link href="https://fonts.googleapis.com/css2?family=K2D:wght@700&display=swap" rel="stylesheet">
-
+  <link href="https://fonts.googleapis.com/css2?family=K2D:wght@700&display=swap"rel="stylesheet">
+  <link rel="stylesheet" type="text/css" href="style_btn.css">
 </head>
 <body style="height:1500px; font-family: 'K2D', sans-serif;">
  <?php 
@@ -22,10 +22,17 @@ require_once('navbar.php');
     <h1>Equipment Borrowed System(EBS)=>admin</h1>      
   </div>  
 </div>
+
 <div class="container">
   <div class="row">
     <div class="col-md-12">
-   
+        <a href="#" class="button">ให้สิทธิ์การใช้งาน</a>
+        <a href="#" class="button">บันทึกข้อมูล/จำนวนเครื่องมือ/อุปกรณ์/วัสดุสิ้นเปลือง</a>
+        <a href="#" class="button">อุปกรณ์ชำรุด</a>
+        <a href="#" class="button">ส่งซ่อมเครื่องมือ/อุปกรณ์</a>
+        <a href="#" class="button">ปรับสถานะเครื่องมือ/อุปกรณ์ที่ซ่อมเสร็จแล้ว</a>
+        <a href="#" class="button">จำหน่ายเครื่องมือ/อุปกรณ์</a>
+        <a href="#" class="button">รายงานทั้งหมด</a>
 
     </div>
   </div>
