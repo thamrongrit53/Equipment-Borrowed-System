0a155355a1f545ef7ad53dd1b09f961b9fdd8d18
thamrongrit53 <47946165+thamrongrit53@users.noreply.github.com>
2020-06-13 01:51:45 +0700

    manager

---
+++ notify_repair_tool_SQL_manager.php
@@ -0,0 +1,30 @@
+<?php
+require_once('condb.php');
+require_once('session_manager.php');
+
+$code=$_POST['code'];
+$name_t=$_POST['name_t'];
+$unit_r=$_POST['unit_r'];
+$m_unit_m=$_POST['m_unit_m'];
+$date_sub=$_POST['date_sub'];
+$location_r=$_POST['location_r'];
+$tel=$_POST['tel'];
+$name_u=$_POST['name_u'];
+$status_r=$_POST['status_r'];
+
+
+ $sql="INSERT INTO `repair`(`code`, `name_t`, `unit_r`, `m_unit_m`, `date_sub`, `location_r`, `tel`, `name_u`, `status_r`) VALUES ('$code','$name_t','$unit_r','$m_unit_m','$date_sub','$location_r','$tel','$name_u','$status_r')";
+
+   $query=mysqli_query($condb,$sql);   
+	if ($query){
+		 $upsql="UPDATE `tb_tool` SET `status`='$status_r' WHERE code='$code'";
+		$queryup=mysqli_query($condb,$upsql);
+     	header("location:notify_repair_tool_manager.php");
+      			}else{
+      	header("location:error_process.php");
+      			}
+ 
+ 
+
+     
+?>
---
+++ notify_repair_tool_manager.php
@@ -0,0 +1,92 @@
+<?php
+require_once('condb.php');
+ require_once('session_manager.php');
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>Equipment Borrowed System(EBS)</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js"></script>
+  <link href="https://fonts.googleapis.com/css2?family=K2D:wght@700&display=swap" rel="stylesheet">
+  <style type="text/css">
+   footer {
+   position: fixed;
+   left: 0;
+   bottom: 0;
+   width: 100%;
+   color: white;
+   text-align: center;
+}
+a{
+  color: white;
+}
+  </style>
+}
+</head>
+<body style="height:1500px; font-family: 'K2D', sans-serif;">
+ <?php 
+ //navbar
+require_once('navbar.php'); 
+ ?>
+
+<div class="container" style="margin-top:50px">
+  <div class="jumbotron">
+    <h1>Equipment Borrowed System(EBS)=>manager</h1>      
+  </div>  
+    <div class="row">
+       <div class="col-md-12">
+            <button class="btn btn-warning"><a href="manager_sys.php"> กลับ-เมนู</a>
+</button> <h3 align="center">รายการอุปกรณ์/เครื่องมือ</h3><br />
+   <div class="form-group">
+    <div class="input-group">
+     <span class="input-group-addon">ค้นหา</span>
+     <input type="text" name="search_text" id="search_text" placeholder="ประเภทอุปกรณ์" class="form-control" />
+    </div>
+   </div>
+      <div id="result"></div>
+
+    </div>
+    </div>
+</div>
+ <footer class="py-4 bg-dark text-white-50" style="margin-top: 20px;">
+    <div class="container text-center">
+      <small>Copyright &copy; SBAC &nbsp;by &nbsp;ไม่นอน</small>
+    </div>
+  </footer>
+</body>
+</html>
+<script>
+$(document).ready(function(){
+
+ load_data();
+
+ function load_data(query)
+ {
+  $.ajax({
+   url:"fetch_repair_tool_manager.php",
+   method:"POST",
+   data:{query:query},
+   success:function(data)
+   {
+    $('#result').html(data);
+   }
+  });
+ }
+ $('#search_text').keyup(function(){
+  var search = $(this).val();
+  if(search != '')
+  {
+   load_data(search);
+  }
+  else
+  {
+   load_data();
+  }
+ });
+});
+</script>
